4df638963ea48910aba326ce2c1c6889ddfc428a
Hayden Young <haydenyoung@wijiti.com>
2012-09-04 18:49:01 +0800

    A REST API client for JSpace (and other Wijiti apps requiring REST functionality).

---
+++ libraries/jrest/client/client.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * A RESTful API client.
+ * 
+ * @package		JRest
+ * @copyright	Copyright (C) 2011-2012 Wijiti Pty Ltd. All rights reserved.
+ * @license     This file is part of the JRest library for Joomla!.
+
+   The JRest library for Joomla! is free software: you can redistribute it 
+   and/or modify it under the terms of the GNU General Public License as 
+   published by the Free Software Foundation, either version 3 of the License, 
+   or (at your option) any later version.
+
+   The JRest library for Joomla! is distributed in the hope that it will be 
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the JRest library for Joomla!.  If not, see 
+   <http://www.gnu.org/licenses/>.
+
+ * Contributors
+ * Please feel free to add your name and email (optional) here if you have 
+ * contributed any source code changes.
+ * Name							Email
+ * Hayden Young					<haydenyoung@wijiti.com> 
+ * 
+ */
+
+class JRestClient
+{
+	protected $url;
+	protected $verb;
+	protected $requestBody;
+	protected $requestLength;
+	protected $username;
+	protected $password;
+	protected $acceptType;
+	protected $responseBody;
+	protected $responseInfo;
+	
+	public function __construct ($url = null, $verb = 'GET', $requestBody = null)
+	{
+		$this->url				= $url;
+		$this->verb				= strtoupper($verb);
+		$this->requestBody		= $requestBody;
+		$this->requestLength	= 0;
+		$this->username			= null;
+		$this->password			= null;
+		$this->acceptType		= 'application/json';
+		$this->responseBody		= null;
+		$this->responseInfo		= null;
+	}
+	
+	public function flush ()
+	{
+		$this->requestBody		= null;
+		$this->requestLength	= 0;
+		$this->verb				= 'GET';
+		$this->responseBody		= null;
+		$this->responseInfo		= null;
+	}
+	
+	public function execute ()
+	{
+		$ch = curl_init();
+		$this->setAuth($ch);
+		
+		try
+		{
+			switch (strtoupper($this->verb))
+			{
+				case 'GET':
+					$this->executeGet($ch);
+					break;
+				case 'POST':
+					$this->executePost($ch);
+					break;
+				case 'PUT':
+					$this->executePut($ch);
+					break;
+				case 'DELETE':
+					$this->executeDelete($ch);
+					break;
+				default:
+					throw new InvalidArgumentException('Current verb (' . $this->verb . ') is an invalid REST verb.');
+			}
+
+			// if debugging is on, or the request was not successful, log the problem.
+			$httpCode = JArrayHelper::getValue($this->getResponseInfo(), "http_code");			
+			
+			if (JDEBUG || array_search($httpCode, array(200, 201, 204)) === false) {				
+				$entry = array(
+					"c-ip"=>$httpCode, 
+					"comment"=>
+						$this->url.'\n'.
+						strtoupper($this->verb).'\n'.
+						$this->requestBody.'\n'.
+						$this->getResponseBody()
+				);
+				
+				JLog::getInstance("jspace.error.php")->addEntry($entry);
+			}	
+		}
+		catch (InvalidArgumentException $e)
+		{
+			curl_close($ch);
+			throw $e;
+		}
+		catch (Exception $e)
+		{
+			curl_close($ch);
+			throw $e;
+		}
+		
+	}
+	
+	protected function executeGet ($ch)
+	{		
+		$this->doExecute($ch);	
+	}
+	
+	protected function executePost ($ch)
+	{		
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this->requestBody);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		
+		$this->doExecute($ch);	
+	}
+	
+	protected function executePut ($ch)
+	{
+		if (!is_string($this->requestBody))
+		{
+			$this->buildPostBody();
+		}
+		
+		$this->requestLength = strlen($this->requestBody);
+		
+		$fh = fopen('php://memory', 'rw');
+		fwrite($fh, $this->requestBody);
+		rewind($fh);
+		
+		curl_setopt($ch, CURLOPT_INFILE, $fh);
+		curl_setopt($ch, CURLOPT_INFILESIZE, $this->requestLength);
+		curl_setopt($ch, CURLOPT_PUT, true);
+		
+		$this->doExecute($ch);
+		
+		fclose($fh);
+	}
+	
+	protected function executeDelete ($ch)
+	{
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+		
+		$this->doExecute($ch);
+	}
+	
+	protected function doExecute (&$curlHandle)
+	{
+		$this->setCurlOpts($curlHandle);
+		$this->responseBody = curl_exec($curlHandle);
+		$this->responseInfo	= curl_getinfo($curlHandle);
+		
+		curl_close($curlHandle);
+	}
+	
+	protected function setCurlOpts (&$curlHandle)
+	{
+		curl_setopt($curlHandle, CURLOPT_TIMEOUT, 10);
+		curl_setopt($curlHandle, CURLOPT_URL, $this->url);
+		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array ('Accept: ' . $this->acceptType, 'Content-Type: ' . $this->acceptType));
+	}
+	
+	protected function setAuth (&$curlHandle)
+	{
+		if ($this->username !== null && $this->password !== null)
+		{
+			curl_setopt($curlHandle, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+			curl_setopt($curlHandle, CURLOPT_USERPWD, $this->username . ':' . $this->password);
+		}
+	}
+	
+	public function getRequestBody()
+	{
+		return $this->requestBody;
+	}
+	
+	public function setRequestBody($requestBody)
+	{
+		$this->requestBody = $requestBody;
+	}
+	
+	public function getAcceptType ()
+	{
+		return $this->acceptType;
+	} 
+	
+	public function setAcceptType ($acceptType)
+	{
+		$this->acceptType = $acceptType;
+	} 
+	
+	public function getPassword ()
+	{
+		return $this->password;
+	} 
+	
+	public function setPassword ($password)
+	{
+		$this->password = $password;
+	} 
+	
+	public function getResponseBody ()
+	{
+		return $this->responseBody;
+	} 
+	
+	public function getResponseInfo ()
+	{
+		return $this->responseInfo;
+	} 
+	
+	public function getUrl ()
+	{
+		return $this->url;
+	} 
+	
+	public function setUrl ($url)
+	{
+		$this->url = $url;
+	} 
+	
+	public function getUsername ()
+	{
+		return $this->username;
+	} 
+	
+	public function setUsername ($username)
+	{
+		$this->username = $username;
+	} 
+	
+	public function getVerb ()
+	{
+		return $this->verb;
+	} 
+	
+	public function setVerb ($verb)
+	{
+		$this->verb = $verb;
+	} 
+}
