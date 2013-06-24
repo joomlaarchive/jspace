9110286c71ae80d272f9a0b26e52912f2c5d6862
Michał Kocztorz <michal.kocztorz@polcode.pl>
2013-06-24 09:02:41 +0200

    connector functionality moved to parent class

--- libraries/jspace/repository/connector.php
+++ libraries/jspace/repository/connector.php
@@ -223,4 +223,93 @@ abstract class JSpaceRepositoryConnector implements JSpaceConnectable
 	 * repository.
 	 */
 	public abstract function execute($endpoint, $action, $useCache = false);
+	
+	protected function _execute($endpoint, $action, $useCache = false) {
+		JSpaceLogger::log('Executing endpoint ' . $endpoint->get('url'));
+		$response = null;
+		
+		if( $useCache ) {
+			JSpaceLogger::log('Using cache');
+			//create a cache key
+			$repository = JSpaceFactory::getRepository();
+			if( $repository->hasCache() ) {
+				JSpaceLogger::log('Repository uses cache');
+				$cacheKey = JSpaceFactory::getCacheKey($endpoint, JArrayHelper::getValue($this->options, 'url'));
+				$key = (string) $cacheKey;
+				// 				$key = md5( JArrayHelper::getValue($this->options, 'url') . serialize( $endpoint ) );
+				JSpaceLogger::log('Cache key: ' . $key);
+				$cachedResponse = $repository->getCache()->get( $cacheKey );
+				if( !is_null( $cachedResponse ) ) {
+					JSpaceLogger::log('Found in cache. Returning.');
+					return $cachedResponse;
+				}
+				JSpaceLogger::log('Not found in cache.');
+			}
+		}
+		
+		try {
+			$url = new JURI(JArrayHelper::getValue($this->options, 'url').'/'.$endpoint->get('url'));
+		
+			if( !is_null($endpoint->get('vars')) ){
+				foreach ($endpoint->get('vars') as $var=>$value) {
+					$url->setVar($var, $value);
+				}
+			}
+				
+			JSpaceLogger::log($url);
+// 			JSpaceLogger::log(print_r($endpoint->get('vars'),true));
+				
+			$client = new JRestClient((string)$url, $action);
+				
+				
+			if (!$endpoint->get('anonymous')) {
+				JSpaceLogger::log("Not anonymous");
+				$client->setUsername(JArrayHelper::getValue($this->options, 'username'));
+				$client->setPassword(JArrayHelper::getValue($this->options, 'password'));
+			}
+			else {
+				JSpaceLogger::log("Anonymous");
+			}
+		
+			JSpaceLogger::log("Request data: " . print_r($endpoint->get('data'),true));
+			if (!is_null($endpoint->get('data'))) {
+				$client->setRequestBody($endpoint->get('data'));
+			}
+				
+			$client->setTimeout( $endpoint->get('timeout') );
+				
+			$client->execute();
+		
+			$info = $client->getResponseInfo();
+			$code = intval(JArrayHelper::getValue($info, "http_code", 0));
+		
+			switch ($code) {
+				case 200:
+				case 201:
+					$response = $client->getResponseBody();
+					JSpaceLogger::log($response);
+					if( $useCache && $repository->hasCache() ) {
+						JSpaceLogger::log('Setting cache. Key: ' . $key);
+						$repository->getCache()->set($cacheKey, $response);
+					}
+					break;
+						
+				case 204:
+					break;
+						
+				default:
+					JSpaceLogger::log($response);
+					$msg = JText::_('JLIB_JSPACE_CONNECTION_ERROR_'.$code);
+					throw new Exception($msg, $code);
+					break;
+			}
+				
+			$client->flush();
+		} catch (Exception $e) {
+			JLog::add($e->getCode().": ".$e->getMessage(), JLog::ERROR, 'library');
+			throw $e;
+		}
+		
+		return $response;
+	}
 }
