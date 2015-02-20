05ea23b219349025b1a35f21a203080641a0d4c5
Hayden Young <hayden@knowledgearc.com>
2015-02-20 21:00:57 +0100

    Uninstall JSpace tables.

--- components/com_jspace/admin/sql/uninstall.mysql.utf8.sql
+++ components/com_jspace/admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,15 @@
 DELETE FROM `#__content_types` WHERE `type_alias` LIKE 'com_jspace.%';
+
+DROP TABLE IF EXISTS `#__jspace_records`;
+
+DROP TABLE IF EXISTS `#__jspace_record_identifiers`;
+
+DROP TABLE IF EXISTS `#__jspace_record_ancestors`;
+
+DROP TABLE IF EXISTS `#__jspace_assets`;
+
+DROP TABLE IF EXISTS `#__jspace_references`;
+
+DROP TABLE IF EXISTS `#__jspace_harvests`;
+
+DROP TABLE IF EXISTS `#__jspace_cache`;
