bec70fab4e48ca24a2538329fc4e5d9fa4633b86
Hayden Young <hayden@knowledgearc.com>
2015-02-21 02:03:23 +0100

    Remove schema.

--- components/com_jspace/admin/sql/install.mysql.utf8.sql
+++ components/com_jspace/admin/sql/install.mysql.utf8.sql
@@ -107,16 +107,16 @@ CREATE TABLE IF NOT EXISTS `#__jspace_harvests` (
 -- A holding table for records harvested.
 
 CREATE TABLE IF NOT EXISTS `#__jspace_cache` (
-    `id` VARCHAR(255), 
-    `data` TEXT NULL, 
-    `harvest_id` INTEGER NOT NULL DEFAULT 0, 
+    `id` VARCHAR(255),
+    `data` TEXT NULL,
+    `harvest_id` INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY(`id`, `harvest_id`),
     KEY `idx_jspace_cache_harvest_id` (`harvest_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__content_types` (`type_id`, `type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`, 
-`content_history_options`) 
-VALUES 
+INSERT INTO `#__content_types` (`type_id`, `type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`,
+`content_history_options`)
+VALUES
 (NULL,
 'JSpace Category',
 'com_jspace.category',
@@ -124,32 +124,32 @@ VALUES
 "dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
 '',
 '{"common":{"core_content_item_id":"id","core_title":"title","core_state":"published","core_alias":"alias",
-"core_created_time":"created_time","core_modified_time":"modified_time","core_body":"description", 
-"core_publish_up":"null","core_publish_down":"null","core_access":"access", "core_params":"params", 
-"core_featured":"null", "core_metadata":"metadata", "core_language":"language", "core_images":"null", 
-"core_urls":"null", "core_version":"version", "core_ordering":"null", "core_metakey":"metakey", 
-"core_metadesc":"metadesc", "core_catid":"parent_id", "core_xreference":"null", "asset_id":"asset_id"}, 
+"core_created_time":"created_time","core_modified_time":"modified_time","core_body":"description",
+"core_publish_up":"null","core_publish_down":"null","core_access":"access", "core_params":"params",
+"core_featured":"null", "core_metadata":"metadata", "core_language":"language", "core_images":"null",
+"core_urls":"null", "core_version":"version", "core_ordering":"null", "core_metakey":"metakey",
+"core_metadesc":"metadesc", "core_catid":"parent_id", "core_xreference":"null", "asset_id":"asset_id"},
 "special":{"parent_id":"parent_id","lft":"lft","rgt":"rgt","level":"level","path":"path","extension":"extension",
 "note":"note"}}',
 'JSpaceHelperRoute::getCategoryRoute',
-'{"formFile":"administrator\/components\/com_categories\/models\/forms\/category.xml", 
-"hideFields":["asset_id","checked_out","checked_out_time","version","lft","rgt","level","path","extension"], 
-"ignoreChanges":["modified_user_id", "modified_time", "checked_out", "checked_out_time", "version", 
-"path"],"convertToInt":["publish_up", "publish_down"], 
+'{"formFile":"administrator\/components\/com_categories\/models\/forms\/category.xml",
+"hideFields":["asset_id","checked_out","checked_out_time","version","lft","rgt","level","path","extension"],
+"ignoreChanges":["modified_user_id", "modified_time", "checked_out", "checked_out_time", "version",
+"path"],"convertToInt":["publish_up", "publish_down"],
 "displayLookup":[{"sourceColumn":"created_user_id","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},
 {"sourceColumn":"access","targetTable":"#__viewlevels","targetColumn":"id","displayColumn":"title"},{
 "sourceColumn":"modified_user_id","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},{
 "sourceColumn":"parent_id","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}]}'
 ),
-(NULL, 
-'JSpace Record', 
-'com_jspace.record', 
+(NULL,
+'JSpace Record',
+'com_jspace.record',
 '{"special":{"dbtable":"#__jspace_records","key":"id","type":"Record","prefix":"JSpaceTable","config":"array()"},"common":{
-"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}', 
-'', 
-'{"common":{"core_content_item_id":"id","core_title":"title","core_state":"state","core_alias":"alias","core_created_time":"created","core_modified_time":"modified","core_body":"description", "core_publish_up":"publish_up","core_publish_down":"publish_down","core_access":"access", "core_params":"params", "core_featured":"featured", "core_language":"language", "core_images":"images", "core_urls":"urls", "core_version":"version", "core_ordering":"ordering", "core_metakey":"metakey", "core_metadesc":"metadesc", "core_catid":"catid", "core_xreference":"xreference", "asset_id":"asset_id"}, "special":{"metadata":"metadata"}}', 
-'JSpaceHelperRoute::getRecordRoute', 
-'{"formFile":"administrator\/components\/com_jspace\/models\/forms\/record.xml", "hideFields":["metadata","asset_id","checked_out","checked_out_time","version","lft","rgt","level","path"],"ignoreChanges":["modified_by", "modified", "checked_out", "checked_out_time", "version", "lft","rgt","level","path"],"convertToInt":["publish_up", 
+"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
+'',
+'{"common":{"core_content_item_id":"id","core_title":"title","core_state":"state","core_alias":"alias","core_created_time":"created","core_modified_time":"modified","core_body":"description", "core_publish_up":"publish_up","core_publish_down":"publish_down","core_access":"access", "core_params":"params", "core_featured":"featured", "core_language":"language", "core_images":"images", "core_urls":"urls", "core_version":"version", "core_ordering":"ordering", "core_metakey":"metakey", "core_metadesc":"metadesc", "core_catid":"catid", "core_xreference":"xreference", "asset_id":"asset_id"}, "special":{"metadata":"metadata"}}',
+'JSpaceHelperRoute::getRecordRoute',
+'{"formFile":"administrator\/components\/com_jspace\/models\/forms\/record.xml", "hideFields":["metadata","asset_id","checked_out","checked_out_time","version","lft","rgt","level","path"],"ignoreChanges":["modified_by", "modified", "checked_out", "checked_out_time", "version", "lft","rgt","level","path"],"convertToInt":["publish_up",
 "publish_down","ordering"],"displayLookup":[{"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id",
 "displayColumn":"title"},{"sourceColumn":"created_by","targetTable":"#__users","targetColumn":"id",
 "displayColumn":"name"},{"sourceColumn":"access","targetTable":"#__viewlevels","targetColumn":"id",
